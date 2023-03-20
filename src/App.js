4202fc04d08d438578b4e59d0491b6edff52a2e2
Petrone22 <the1rashid@gmai.com>
2023-03-20 18:32:21 +0200

    set up landing page.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
-
+import React, { useState } from "react";
+import Intro from "./components/intro";
 function App() {
-  return "hi";
+  const [display, setDisplay] = useState(true);
+  console.log(display);
+  return (
+    <div
+      className="main"
+      onKeyDown={() => {
+        setDisplay((prevDisp) => !prevDisp);
+      }}
+    >
+      <Intro display={display} setDisplay={setDisplay} />
+    </div>
+  );
 }
 
 export default App;
